acfad94e96fc2c538c971ca655d0998c56b4718f
Quoc Minh Bui <bqmxnh@gmail.com>
2024-05-03 10:48:23 +0700

    - Add Lobby function - Handles creation and connection to Lobby - Manage player connections in Lobby - Send notifications between Host and Guest - Handle Lobby statuses - Send player movement messages in the Lobby - Handled at the end of the game

--- Server/Server/Player.cs
+++ Server/Server/Player.cs
@@ -7,12 +7,11 @@ using System.Net.Sockets;
 
 namespace Server
 {
-    // Lớp chứa thông tin người chơi
     internal class Player
     {
-        public string name { get; set; } // Tên Player
-        public int isHost { get; set; } // Xác định nó có phải là máy chủ ko
-        public int turn { get; set; } // Xác định lượt chơi, 1 nếu là lượt chơi của mình, 0 nếu ko là lượt chơi của mình 
-        public Socket playerSocket { get; set; } //Kết nối socket của người chơi với server, giao tiếp, gửi và nhận dữ liệu của server với client
+        public string name { get; set; }
+        public int isHost { get; set; }
+        public int turn { get; set; }
+        public Socket playerSocket { get; set; }
     }
 }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -6,201 +6,251 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
-using System.Numerics;
 
 namespace Server
 {
     class Program
     {
-        // Đối tượng Socket lắng nghe kết nối từ Client
         private static Socket serverSocket;
-        // Đối tượng Socket đại diện cho các Client khi kết nối
         private static Socket client;
-        // Luồng xử lý dữ liệu từ Client
         private static Thread clientThread;
-        // Một danh sách đại diện cho các người chơi đã kết nối
         private static List<Player> connectedPlayers = new List<Player>();
+        private static List<Lobby> lobbies = new List<Lobby>();
+        public static string host;
 
         static void Main(string[] args)
         {
-            // Tạo một máy chủ lắng nghe các kết nối đến cổng 9999
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            // Gắn Socket với địa chỉ và cổng
             serverSocket.Bind(new IPEndPoint(IPAddress.Any, 9999));
-            // Bắt đầu lắng nghe các kết nối, ở đây cụ thể là 2
             serverSocket.Listen(2);
             Console.WriteLine("Wating for player");
 
-            // Đây là một vòng lặp vô hạn đợi các kết nối từ Client
+
             while (true)
             {
-                // Trả về một đối tượng Socket (client) đại diện cho kết nối mới được chấp nhận
                 client = serverSocket.Accept();
-                // In ra thông điệp bao gồm IP và cổng 
                 Console.WriteLine("Player connected from " + client.RemoteEndPoint.ToString());
-                // Một luồng mới được tạo để xử lý Client vừa kết nối, đồng thời truyền một hàm readClientSocket để xử lý dữ liệu từ Client này
                 clientThread = new Thread(() => readClientSocket(client));
-                // Nếu luồng chính của chương trình kết thúc, luồng này cũng sẽ kết thúc mà không cần đợi đến khi hoàn thành nhiệm vụ
                 clientThread.IsBackground = true;
-                // Luồng mới được tạo, bắt đầu thực thi hàm
                 clientThread.Start();
             }
         }
-        // Hàm readClientSocket để xử lý dữ liệu từ Client
+
         public static void readClientSocket(Socket client)
         {
-            // Một player được tạo đại diện cho Client vừa kết nối
             Player player = new Player();
-            // Socket của Client được gán cho thuộc tính playerSocket của Player
+            Lobby lobby = new Lobby();
             player.playerSocket = client;
-            // Thêm player vào danh sách connectedPlayer
+            lobby.ip = (((IPEndPoint)client.RemoteEndPoint).Address).ToString();
+            lobbies.Add(lobby);
             connectedPlayers.Add(player);
-            // Khởi tạo một mảng byte
+
             byte[] bytes = new byte[1024];
-            // Vòng lặp đọc dữ liệu từ player, nó sẽ chạy cho đến khi Player bị đóng hoặc bị ngắt
+
             while (player.playerSocket.Connected)
             {
-                // Kiểm tra có dữ liệu mới từ Client hay không bằng cách dùng Available
                 if (player.playerSocket.Available > 0)
                 {
-                    // Khởi tạo biến msg
                     string msg = "";
 
                     while (player.playerSocket.Available > 0)
                     {
-                        // Nếu có một dữ liệu mới, máy chủ sẽ đọc dữ liệu đó và ghi vào một mảng bytes
                         int bytesRec = player.playerSocket.Receive(bytes);
-                        // Dữ liệu đó sẽ được mã hóa thành chuỗi ASCII, lưu vào biến msg
                         msg += Encoding.ASCII.GetString(bytes, 0, bytesRec);
                     }
-                    // Sau khi nhận đc thông điệp từ Player, nó sẽ in ra thông điệp kèm theo IP và Port
+
                     Console.WriteLine(player.playerSocket.RemoteEndPoint.ToString() + " : " + msg);
-                    // Gọi hàm AnalyzeMessage để xử lý và phân tích thông điệp từ Client
-                    AnalyzeMessage(msg, player);
+                    AnalyzeMessage(msg, player, lobby);
                 }
             }
         }
 
-        public static void AnalyzeMessage(string msg, Player player)
+        public static Lobby findLobby(Lobby lobby)
         {
-            // msg là thông điệp được gưi từ Client đến máy chủ
+            return lobbies.Find(x => x.ip == lobby.ip);
+        }
+
+        public static void AnalyzeMessage(string msg, Player player, Lobby lobby)
+        {
             string[] PayLoad = msg.Split('|');
-            // Loại thông điệp của Client
+
             switch (PayLoad[0])
             {
-                // Payload[0] là CREATE, tên người chơi được lấy từ Payload1, thông điệp có dạng CREATED|PlayerName sẽ được gửi lại cho player để xác nhận rằng đã tạo người chơi thành công
                 case "CREATE":
                     {
-                        player.name = PayLoad[1];
 
-                        byte[] data = Encoding.UTF8.GetBytes("CREATED|" + player.name);
-
-                        player.playerSocket.Send(data);
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
+                        {
+                            string ip = lobby1.ip;
+                            if (lobby1.isHost == false)
+                            {
+                                player.name = PayLoad[1];
+                                lobby1.isHost = true;
+                                lobby1.Host = player;
+                                byte[] data = Encoding.UTF8.GetBytes("CREATED|" + player.name);
+                                player.playerSocket.Send(data);
+                                foreach (var lb in lobbies.ToList())
+                                {
+                                    if (lb.ip == lobby1.ip)
+                                    {
+                                        lobbies.Remove(lb);
+                                        lobbies.Add(lobby1);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Existed");
+                                byte[] data = Encoding.UTF8.GetBytes("EXISTED|");
+                                player.playerSocket.Send(data);
+                            }
+                        }
                     }
                     break;
-                // Payload[0] là CONNECT, tên người chơi được lấy từ Payload[1]
                 case "CONNECT":
                     {
-                        player.name = PayLoad[1];
-                        // Gửi thông điệp của người chơi đã kết nối cho người chơi mới
-                        foreach (var p in connectedPlayers)
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
                         {
-                            byte[] data = Encoding.UTF8.GetBytes("CONNECTED|" + p.name);
-                            player.playerSocket.Send(data);
-                            Thread.Sleep(100);
-                        }
-
-                        // Gửi thông điệp của người chơi mới cho người chơi đã kết nối
-                        foreach (var p in connectedPlayers)
-                        {
-                            if (player.playerSocket != p.playerSocket)
+                            if (lobby1.isHost == true && lobby1.isGuest == false)
                             {
-                                byte[] data = Encoding.UTF8.GetBytes("CONNECTED|" + player.name);
-                                p.playerSocket.Send(data);
+                                player.name = PayLoad[1];
+                                lobby1.isGuest = true;
+                                lobby1.Guest = player;
+                                byte[] data = Encoding.UTF8.GetBytes("CONNECTED|" + lobby1.Host.name);
+                                player.playerSocket.Send(data);
                                 Thread.Sleep(100);
+                                data = Encoding.UTF8.GetBytes("CONNECTED|" + player.name);
+                                player.playerSocket.Send(data);
+                                lobby1.Host.playerSocket.Send(data);
+                                foreach (var lb in lobbies.ToList())
+                                {
+                                    if (lb.ip == lobby1.ip)
+                                    {
+                                        lobbies.Remove(lb);
+                                        lobbies.Add(lobby1);
+                                    }
+                                }
+                            }
+                            else if (lobby1.isHost == false)
+                            {
+                                Console.WriteLine("Not Existed");
+                                byte[] data = Encoding.UTF8.GetBytes("NOTEXISTED|");
+                                player.playerSocket.Send(data);
+                            }
+                            else if (lobby1.isHost == true && lobby1.isGuest == true)
+                            {
+                                Console.WriteLine("Full");
+                                byte[] data = Encoding.UTF8.GetBytes("FULL|");
+                                player.playerSocket.Send(data);
                             }
                         }
                     }
                     break;
                 case "DISCONNECT":
                     {
-                        //Tạo ra 1 bản sao của danh sách các connectedPlayers bằng cách sử dụng phương thức ToList()
-                        foreach (var p in connectedPlayers.ToList())
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
                         {
-                            // Nếu tên của Client trùng với tên của Client trong thông điệp Disconnect ko
-                            if (p.name == PayLoad[1])
+                            if (player.name == lobby1.Host.name)
                             {
-                                // Nếu có, đóng kết nối từ Client và đến Client
-                                p.playerSocket.Shutdown(SocketShutdown.Both);
-                                // Đóng kết nối hoàn toàn bằng Close
-                                p.playerSocket.Close();
-                                // Xóa nó khỏi danh sách các connectedPlayers
-                                connectedPlayers.Remove(p);
+                                lobby1.Host.playerSocket.Shutdown(SocketShutdown.Both);
+                                lobby1.Host.playerSocket.Close();
+                                lobby1.isHost = false;
+                                connectedPlayers.Remove(lobby1.Host);
+                            }
+                            else if (player.name == lobby1.Guest.name)
+                            {
+                                lobby1.Guest.playerSocket.Shutdown(SocketShutdown.Both);
+                                lobby1.Guest.playerSocket.Close();
+                                lobby1.isGuest = false;
+                                connectedPlayers.Remove(lobby1.Guest);
                             }
                         }
                     }
                     break;
                 case "START":
                     {
-                        foreach (var p in connectedPlayers)
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
                         {
-                            // Gửi thông điệp INIT đến mỗi Client được kết nối
-                            // Với mỗi Client trong danh sách ConnectedPlayer, một chuỗi message được tạo với nội dung INIT| để bắt đầu (Khởi tạo)
-                            // Vòng lặp duyệt qua từng Client để thêm vào chuối message 
-                            // Nó được mã hóa bằng Encoding UTF8
-                            // Mảng bytes này sẽ được gởi đến các Client thông qua Socket mỗi CLient
-                            // Cuối cùng, một dòng thông báo Sendback: + message được đưa ra màn hình Console
-                            // Lưu ý, dừng 100ms để đảm bảo quá trình gửi thông điệp
-                            string message = "INIT|";
-                            foreach (var p2 in connectedPlayers)
-                            {
-                                message += p2.name + "|";
-                            }
+                            string message = "HOST|" + lobby1.Host.name + "|" + lobby1.Guest.name;
                             byte[] data = Encoding.UTF8.GetBytes(message);
-                            p.playerSocket.Send(data);
+                            lobby1.Host.playerSocket.Send(data);
+                            Console.WriteLine("Sendback: " + message);
+                            Thread.Sleep(100);
+
+                            message = "INIT|" + lobby1.Host.name + "|" + lobby1.Guest.name;
+                            data = Encoding.UTF8.GetBytes(message);
+                            lobby1.Guest.playerSocket.Send(data);
                             Console.WriteLine("Sendback: " + message);
                             Thread.Sleep(100);
                         }
                     }
                     break;
-
-                // Thông điệp là RIGHT, LEFT là dịch phải, trái, nó chỉ dịch chuyển khi tên của Client không cùng với tên được chỉ định trong thông điệp
-                // Thông điệp sẽ có dạng RIGHT/LEFT|Giá trị|Vị trí|player1turn
-                // Mã hóa thông điệp dạng UTF8
-                // Cuối cùng, gửi thông điệp đó đến Client tương ứng
+                case "TIMEOUT":
+                    {
+                        Console.WriteLine("Timeout: " + PayLoad[1]);
+                    }
+                    break;
+                case "CLOSE":
+                    {
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
+                        {
+                            Console.WriteLine("Close: " + PayLoad[1]);
+                            string message = "CLOSED|" + PayLoad[1];
+                            byte[] data = Encoding.UTF8.GetBytes(message);
+                            if (lobby1.Host.name == PayLoad[1])
+                            {
+                                lobby1.Host.playerSocket.Send(data);
+                            }
+                            else if (lobby1.Guest.name == PayLoad[1])
+                            {
+                                lobby1.Guest.playerSocket.Send(data);
+                            }
+                        }
+                    }
+                    break;
                 case "RIGHT":
                     {
-                        string name = PayLoad[4];
-                        foreach (var p in connectedPlayers)
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
                         {
-                            if (p.name != name)
-                            {
-                                string position = PayLoad[2];
-                                string value = PayLoad[1];
-                                string player1turn = PayLoad[3];
-                                string message = "RIGHT|" + value + "|" + position + "|" + player1turn;
-                                byte[] data = Encoding.UTF8.GetBytes(message);
-                                p.playerSocket.Send(data);
-                            }
+                            string position = PayLoad[2];
+                            string value = PayLoad[1];
+                            string player1turn = PayLoad[3];
+                            string message = "GORIGHT|" + value + "|" + position + "|" + player1turn;
+                            byte[] data = Encoding.UTF8.GetBytes(message);
+                            lobby1.Guest.playerSocket.Send(data);
+                            lobby1.Host.playerSocket.Send(data);
                         }
                     }
                     break;
                 case "LEFT":
                     {
-                        string name = PayLoad[4];
-                        foreach (var p in connectedPlayers)
+                        Lobby lobby1 = findLobby(lobby);
+                        if (lobby1 != null)
                         {
-                            if (p.name != name)
-                            {
-                                string position = PayLoad[2];
-                                string value = PayLoad[1];
-                                //Console.WriteLine("Value: " + PayLoad[3]);
-                                string player1turn = PayLoad[3];
-                                string message = "LEFT|" + value + "|" + position + "|" + player1turn;
-                                byte[] data = Encoding.UTF8.GetBytes(message);
-                                p.playerSocket.Send(data);
-                            }
+                            string position = PayLoad[2];
+                            string value = PayLoad[1];
+                            string player1turn = PayLoad[3];
+                            string message = "GOLEFT|" + value + "|" + position + "|" + player1turn;
+                            byte[] data = Encoding.UTF8.GetBytes(message);
+                            lobby1.Guest.playerSocket.Send(data);
+                            lobby1.Host.playerSocket.Send(data);
                         }
+                    }
+                    break;
+                case "WINNER":
+                    {
+                        Console.WriteLine("Winner: " + PayLoad[1]);
+                    }
+                    break;
+                case "DRAW":
+                    {
+                        Console.WriteLine("Draw");
                     }
                     break;
                 default:
